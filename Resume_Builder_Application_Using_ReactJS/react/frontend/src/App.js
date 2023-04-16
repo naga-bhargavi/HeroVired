1f2c0c5431752b35fe2cc4093ba2303251159bff
naga-bhargavi <84374071+naga-bhargavi@users.noreply.github.com>
2023-04-16 15:11:35 +0530

    Add files via upload

---
+++ Resume_Builder_Application_Using_ReactJS/react/frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Routes, Route ,Navigate} from 'react-router-dom';
+import DetailsFillingPage from './Components/Details/DetailsFillingPage';
+import ResumeTemplates from './Components/Home'
+import NavBar from './Components/Navbar'
+import MyResume from './Components/Details/MyResume';
+import AboutUs from './Components/AboutUs';
+import './App.css';
+
+// This component defines routing for other components //
+function App() {
+  return (
+    <div>
+        <div> 
+          <NavBar/>
+        </div>
+        
+        <div>
+            <Routes>
+                  <Route exact path="/" element={<ResumeTemplates/>}></Route>
+                  <Route path="/myresumes" element={<MyResume/>}></Route>
+                  <Route exact path="/about" element={<AboutUs/>}></Route>
+                  <Route path="/detailsfillingpage/*" element ={<DetailsFillingPage />}></Route> 
+                  <Route path="*" element={<Navigate to="/" />}></Route>
+            </Routes> 
+        </div>
+    </div>
+  )
+}
+
+
+export default App
